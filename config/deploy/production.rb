74e6eb8f48a5b798be3fe9d5125b940dbd8c5cfe
Al Fian <al.fian.v001@gmail.com>
2021-01-29 00:17:44 +0700

    Changed IP address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,7 +60,7 @@
 #     # password: "please use keys"
 #   }
 
-server '54.169.131.35', user: 'ubuntu', roles: %w{web app db}
+server '172.31.43.111', user: 'ubuntu', roles: %w{web app db}
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w[publickey],
